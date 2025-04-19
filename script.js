35aade00c32da6e17d031812b0e226519545ecbf
Erjon Lazellari <elazellari@myseneca.ca>
2025-04-19 00:45:06 -0400

    add book button and function, display the book library

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-const myLibrary = [
-  { id: 1, title: "New Book", author: "Hajvani" },
-  { id: 2, title: "Kite Runner", author: "Khaled" },
-  { id: 3, title: "3 Friends", author: "Remark" },
-];
+//empty book library array
+let myLibrary = [];
 
 //Book Constructor
 function Book(title, author) {
@@ -12,7 +9,6 @@ function Book(title, author) {
 }
 
 //Function to create a book and store it into the array
-
 function addBookToLibrary(title, author) {
   const book = new Book(title, author);
   myLibrary.push(book);
@@ -22,9 +18,30 @@ function addBookToLibrary(title, author) {
 
 const table = document.querySelector("table");
 
-//function that loops through the array and displays book
+//Display form button
+const addBook = document.querySelector(".add-book");
+const bookForm = document.querySelector(".book-form");
+addBook.addEventListener("click", (event) => {
+  event.preventDefault();
+  bookForm.style.display = "block";
+  addBook.style.display = "none";
+});
 
+//add new book to library
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const submit = document.querySelector(".submit");
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  addBookToLibrary(title.value, author.value);
+  displayBook(myLibrary);
+  title.value = "";
+  author.value = "";
+});
+
+//function that loops through the array and displays book
 function displayBook(library) {
+  table.innerHTML = ``;
   library.forEach((book) => {
     const tr = document.createElement("tr");
     table.appendChild(tr);
@@ -33,12 +50,3 @@ function displayBook(library) {
     tr.appendChild(td);
   });
 }
-
-//Display form button
-const addBook = document.querySelector(".add-book");
-const bookForm = document.querySelector(".book-form");
-
-addBook.addEventListener("click", () => {
-  bookForm.style.display = "block";
-  addBook.style.display = "none";
-});
